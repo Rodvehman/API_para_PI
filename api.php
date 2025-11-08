194ac9e9f131cc968e6747168c41c9f13fe90d27
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-08 17:43:41 -0300

    Update API

--- api.php
+++ api.php
@@ -4,25 +4,65 @@
 
     $metodo = $_SERVER['REQUEST_METHOD'];
 
-    $url = 'avioes.json';
+    // Leitura do arquivo JSON
+    $arquivo = 'avioes.json';
+    $conteudo = file_get_contents($arquivo);
 
-    $conversao = file_get_contents($url);
-
-    $aviao = json_decode($conversao, true);
-
-    $id = $_GET['id'];
-
-    if ($aviao['avioes'][$id]){
-        echo "Modelo: ".$aviao['avioes'][$id]['modelo']."\n";
-        echo "Fabricante: ".$aviao['avioes'][$id]['fabricante']."\n";
-        echo "Velocidade de Cruzeiro: ".$aviao['avioes'][$id]['velocidade_cruzeiro_km_h']." km/h\n";
-        echo "Capacidade: ".$aviao['avioes'][$id]['capacidade_passageiros']."\n";
-        echo "Autonomia: ".$aviao['avioes'][$id]['autonomia_km']." Kms\n";
-        echo "Em Operação Por: ".$aviao['avioes'][$id]['companhia_exemplo']."\n";
-        echo "Início de Operação: ".$aviao['avioes'][$id]['ano_primeiro_voo']."\n";
-        echo "Situação: ".$aviao['avioes'][$id]['status']."\n";
-    } else {
-        echo "Para especificar um modelo, insira '?', 'id=' e o número do modelo desejado.<br>";
-        echo "Segue a lista completa.";
-        echo var_dump($aviao['avioes']);
+    // Validação se o arquivo existe (ou se deu a carga corretamente)
+    if ($conteudo === false){
+        http_response_code(500);
+        echo (["erro => Arquivo 'avioes.json' não encontrado."]);
+        exit;
     }
+
+    // Converte o JSON
+    $dados = json_decode($conteudo, true);
+    if (json_last_error() !== JSON_ERROR_NONE){
+        http_response_code(500);
+        echo json_encode(["erro => Formato JSON inválido."]);
+        exit;
+    }
+
+    $avioes = $dados['avioes'] ?? [];
+
+    // Pega o parâmetro da URL
+    $id = $_GET['id'] ?? null;
+    $fabricante = $_GET['fabricante'] ?? null;
+
+    // aplica o filtro
+    $resultado = [];
+
+    if ($id !== null){
+        $id = (int) $id;
+        foreach ($avioes as $aviao){
+            if ($aviao['id'] == $id){
+                $resultado = $aviao;
+                break;
+            }
+        }
+    } else if ($fabricante !== null){
+        $fabricante = trim($fabricante);
+        foreach ($avioes as $aviao) {
+            if (strcasecmp($aviao['fabricante'], $fabricante) === 0){
+                $resultado[] = $aviao;
+            }
+        }
+    } else {
+        $resultado = $avioes;
+    }
+
+    if (empty($resultado)){
+        http_response_code(404);
+        echo json_encode([
+            "mensagem" => "Nenhum avião localizado",
+            "filtro usados" => [
+                "id" => $id,
+                "fabricante" => $fabricante
+            ]
+        ]);
+        exit;
+    }
+
+    // === 5. Retorna em JSON bonito ===
+    echo json_encode($resultado, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+?>
