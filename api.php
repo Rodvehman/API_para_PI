409b836f95037cfe93bf11787b6786ec2edab8b9
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-08 15:39:06 -0300

    Update API

--- api.php
+++ api.php
@@ -11,11 +11,20 @@
     $aviao = json_decode($conversao, true);
 
     $id = $_GET['id'];
+    $fabricante = $_GET['fabricante'];
+
+    try {
+        
+    } catch (\Throwable $e) {
+        echo 'Parâmetro não localizado. Erro: '.$e->
+        print_r($aviao['avioes']);
+    }
 
     if ($id === null){
-        print_r($aviao['avioes']);
-    } else {   
+    } else if ($id !== null) {   
         echo var_dump($aviao['avioes'][$id]);
+    } else if ($){
+        echo var_dump(($aviao['avioes'][$fabricante]));
     }
 
 
